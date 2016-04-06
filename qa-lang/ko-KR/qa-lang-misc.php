0e6ff43f8518354793fcc433a66b36eddd6eb03f
Jeremy Bae <jswbae@gmail.com>
2016-04-07 02:07:28 +0900

    Add Korean translation files

---
+++ qa-lang/ko-KR/qa-lang-misc.php
@@ -0,0 +1,124 @@
+<?php
+/*
+	Question2Answer by Gideon Greenspan and contributors
+	http://www.question2answer.org/
+
+	File: qa-include/qa-lang-misc.php
+	Description: Miscellaneous language phrases
+
+
+	This program is free software; you can redistribute it and/or
+	modify it under the terms of the GNU General Public License
+	as published by the Free Software Foundation; either version 2
+	of the License, or (at your option) any later version.
+
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+
+	More about this license: http://www.question2answer.org/license.php
+*/
+
+	return array(
+		'block_ip_button' => 'IP 주소 차단',
+		'browse_categories' => 'Browse categories',
+		'captcha_approve_fix' => 'This verification will stop appearing once your account is approved.',
+		'captcha_confirm_fix' => 'To avoid this verification in future, please ^5confirm your email address^6.',
+		'captcha_error' => '스팸 차단 확인 절차를 완료해주세요',
+		'captcha_label' => '스팸 차단:',
+		'captcha_login_fix' => 'To avoid this verification in future, please ^1log in^2 or ^3register^4.',
+		'feed_a_edited_prefix' => 'Answer edited: ',
+		'feed_a_prefix' => 'Answered: ',
+		'feed_a_reshown_prefix' => 'Answer reshown: ',
+		'feed_a_selected_prefix' => 'Answer selected: ',
+		'feed_c_edited_prefix' => 'Comment edited: ',
+		'feed_c_moved_prefix' => 'Comment moved: ',
+		'feed_c_prefix' => 'Commented: ',
+		'feed_c_reshown_prefix' => 'Comment reshown: ',
+		'feed_closed_prefix' => 'Closed: ',
+		'feed_edited_prefix' => 'Edited: ',
+		'feed_hidden_prefix' => 'Hidden: ',
+		'feed_not_found' => 'Feed not found',
+		'feed_recategorized_prefix' => 'Recategorized: ',
+		'feed_reopened_prefix' => 'Reopened: ',
+		'feed_reshown_prefix' => 'Reshown: ',
+		'feed_retagged_prefix' => 'Retagged: ',
+		'feedback_email' => 'Your email: (optional)',
+		'feedback_empty' => 'Please use this field to send some comments or suggestions',
+		'feedback_message' => 'Your comments or suggestions for ^:',
+		'feedback_name' => 'Your name: (optional)',
+		'feedback_sent' => 'Your message below was sent - thank you.',
+		'feedback_title' => 'Send feedback',
+		'form_security_again' => 'Please click again to confirm',
+		'form_security_reload' => 'Please reload the page then try again',
+		'hide_all_ip_button' => 'Hide all posts from this IP',
+		'host_name' => 'Host name:',
+		'inbox' => '받은 쪽지함',
+		'matches_blocked_ips' => 'Matches blocked IP addresses:',
+		'message_empty' => 'Please enter your message to send to this user',
+		'message_explanation' => 'This will be sent as a notification from ^. Your email address will not be revealed unless you include it in the message.',
+		'message_for_x' => 'Your message for ^:',
+		'message_limit' => 'You cannot send more private messages this hour',
+		'message_must_login' => '쪽지를 보내시려면 ^1로그인^2하시거나 ^3회원 가입^4을 해주세요.',
+		'message_recent_history' => 'Recent correspondence with ^',
+		'message_sent' => 'Your private message below was sent',
+		'more_favorite_qs' => 'More favorite questions...',
+		'more_favorite_tags' => 'More favorite tags...',
+		'more_favorite_users' => 'More favorite users...',
+		'my_favorites_title' => '내 관심 목록',
+		'nav_all_my_updates' => 'All my updates',
+		'nav_my_content' => 'My content',
+		'nav_my_details' => '내 회원정보',
+		'nav_my_favorites' => '내 관심 목록',
+		'nav_user_activity' => '최근 활동 내역',
+		'nav_user_as' => '답변 내역',
+		'nav_user_pms' => '쪽지 관리',
+		'nav_user_qs' => '질문 내역',
+		'nav_user_wall' => 'Wall',
+		'no_activity_from_x' => 'No activity from ^',
+		'no_favorite_categories' => '관심 카테고리가 없습니다',
+		'no_favorite_qs' => '관심 질문이 없습니다',
+		'no_favorite_tags' => '관심 태그가 없습니다',
+		'no_favorite_users' => '관심 사용자가 없습니다',
+		'no_recent_updates' => 'No recent updates',
+		'no_updates_content' => 'No recent updates for my content',
+		'no_updates_favorites' => 'No updates for my favorites',
+		'outbox' => '보낸 쪽지함',
+		'pm_inbox_title' => '받은 쪽지',
+		'pm_outbox_title' => '보낸 쪽지',
+		'private_message_title' => 'Send a private message',
+		'recent_activity_from_x' => 'Recent activity from ^',
+		'recent_updates_content' => 'Recent updates for my content',
+		'recent_updates_favorites' => 'Recent updates for my favorites',
+		'recent_updates_title' => 'Recent updates for me',
+		'site_in_maintenance' => 'This site is currently down for maintenance - please come back soon.',
+		'suggest_favorites_add' => '내 관심 목록에 추가하시려면 관련 페이지에서 ^표시를 눌러주세요.',
+		'suggest_update_favorites' => 'For more updates, add items to ^1your favorites^2.',
+		'unblock_ip_button' => 'IP 주소 차단 해제',
+		'your_a_commented' => 'your answer commented',
+		'your_a_edited' => '수정:',
+		'your_a_hidden' => 'your answer hidden',
+		'your_a_questioned' => 'asked on your answer',
+		'your_a_reshown' => 'your answer reshown',
+		'your_a_selected' => 'your answer selected',
+		'your_c_edited' => 'your comment edited',
+		'your_c_followed' => 'your comment followed',
+		'your_c_hidden' => 'your comment hidden',
+		'your_c_moved' => 'your comment moved',
+		'your_c_reshown' => 'your comment reshown',
+		'your_q_answered' => '답변:',
+		'your_q_closed' => 'your question closed',
+		'your_q_commented' => 'your question commented',
+		'your_q_edited' => 'your question edited',
+		'your_q_hidden' => '숨김:',
+		'your_q_recategorized' => 'your question recategorized',
+		'your_q_reopened' => 'your question reopened',
+		'your_q_reshown' => 'your question reshown',
+		'your_q_retagged' => 'your question retagged',
+	);
+
+
+/*
+	Omit PHP closing tag to help avoid accidental output
+*/
---
+++ qa-lang/ko-KR/qa-lang-users.php
@@ -0,0 +1,142 @@
+<?php
+/*
+	Question2Answer by Gideon Greenspan and contributors
+	http://www.question2answer.org/
+
+	File: qa-include/qa-lang-users.php
+	Description: Language phrases for user management
+
+
+	This program is free software; you can redistribute it and/or
+	modify it under the terms of the GNU General Public License
+	as published by the Free Software Foundation; either version 2
+	of the License, or (at your option) any later version.
+
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+
+	More about this license: http://www.question2answer.org/license.php
+*/
+
+	return array(
+		'about' => '자기소개',
+		'add_user_x_favorites' => 'Add user ^ to my favorites',
+		'approve_required' => '관리자 가입 승인 대기 중입니다.',
+		'approve_title' => '관리자 승인 대기',
+		'approve_user_button' => 'Approve User',
+		'approved_user' => '정회원',
+		'avatar_default' => 'Default',
+		'avatar_gravatar' => '^1Gravatar^2 이용하기',
+		'avatar_label' => '프로필 이미지:',
+		'avatar_none' => '없음',
+		'block_user_button' => 'Block User',
+		'blocked_users' => 'Blocked users',
+		'category_level_add' => ' - ^1add category-specific privileges^2',
+		'category_level_in' => 'for the category below:',
+		'category_level_label' => 'Upgraded to:',
+		'category_level_none' => 'No upgrade',
+		'change_email_link' => ' - ^1이메일 주소 변경^2',
+		'change_password' => '비밀번호 변경',
+		'confirm_complete' => '이메일 본인 확인이 완료되었습니다. 감사합니다.',
+		'confirm_emailed' => '본인 확인 메일이 발송되었습니다. 받으신 이메일의 링크를 눌러주세요.',
+		'confirm_required' => '회원 가입을 마치시려면 등록하신 이메일 주소로 보내드린 가입 확인 링크를 눌러주세요 - ^1가입 확인 메일 다시 보내기^2',
+		'confirm_title' => '이메일 본인 확인',
+		'confirm_wrong_log_in' => '틀린 코드 - 본인 확인 메일을 다시 받으려면 ^1로그인^2해주세요.',
+		'confirm_wrong_resend' => '틀린 코드 - 본인 확인 메일 다시 보내기',
+		'delete_user_button' => 'Delete User',
+		'edit_profile' => '회원 정보 수정',
+		'edit_user_button' => 'Edit User',
+		'email_confirmed' => 'Confirmed',
+		'email_exists' => 'Email already belongs to an account',
+		'email_handle_label' => '이메일 혹은 사용자명:',
+		'email_invalid' => 'Email is invalid - please check carefully',
+		'email_label' => '이메일 주소:',
+		'email_not_confirmed' => 'Not yet confirmed',
+		'email_please_confirm' => 'Please ^5confirm^6',
+		'email_required' => '이메일 주소는 필수 입력 항목입니다',
+		'forgot_link' => '비밀번호를 분실하셨습니까?',
+		'full_name' => '성명',
+		'handle_empty' => '사용자 아이디는 필수 입력 항목입니다',
+		'handle_exists' => 'Username is taken - please try another',
+		'handle_has_bad' => 'Username may not contain: ^',
+		'handle_label' => '사용자 아이디:',
+		'hide_all_user_button' => 'Hide all posts by this user',
+		'last_login_label' => '최종 접속 정보:',
+		'last_write_label' => '최종 게시물 작성 정보:',
+		'level_admin' => '관리자',
+		'level_editor' => 'Editor',
+		'level_expert' => 'Expert',
+		'level_for_category' => '^1 for ^2',
+		'level_in_general' => 'in general',
+		'level_moderator' => 'Moderator',
+		'level_super' => '최고 관리자',
+		'location' => '위치',
+		'log_in_to_access' => 'You may now ^1log in^2 to access your account.',
+		'login_button' => '로그인',
+		'login_limit' => 'Too many login attempts - please try again in an hour',
+		'login_title' => '로그인',
+		'mass_mailings' => 'Mass mailings:',
+		'mass_mailings_explanation' => 'Subscribe to emails sent out to all users',
+		'member_for' => '가입 기간:',
+		'member_type' => '회원 구분:',
+		'new_password_1' => '새로운 비밀번호:',
+		'new_password_2' => '새로운 비밀번호 (재확인):',
+		'no_blocked_users' => 'No blocked users found',
+		'no_permission' => '해당 작업을 수행할 권한이 없습니다',
+		'old_password' => '예전 비밀번호:',
+		'only_shown_admins' => '(only shown to admins)',
+		'only_shown_editors' => '(only shown to editors and above)',
+		'only_shown_experts' => '(only shown to experts and above)',
+		'only_shown_moderators' => '(only shown to moderators and admins)',
+		'password_changed' => '비밀번호가 변경되었습니다',
+		'password_label' => '비밀번호:',
+		'password_min' => '비밀번호는 최소 ^글자 이상으로 설정해주세요',
+		'password_mismatch' => 'New passwords do not match',
+		'password_none' => 'None. To log in directly, set a password below.',
+		'password_sent' => '새로운 비밀번호를 등록하신 이메일 주소로 보내드렸습니다',
+		'password_to_set' => 'Please set on your account page',
+		'password_wrong' => '비밀번호가 틀렸습니다',
+		'private_messages' => '쪽지:',
+		'private_messages_explanation' => '다른 사용자의 쪽지 이메일 받기 (다른 사용자는 여러분의 이메일 주소를 볼 수 없습니다)',
+		'profile_saved' => '회원정보가 저장되었습니다',
+		'register_button' => '가입하기',
+		'register_limit' => 'Too many registrations - please try again in an hour',
+		'register_suspended' => '당분간 신규 회원 가입을 받지 않고 있습니다. 나중에 다시 가입 신청해주세요.',
+		'register_title' => '회원 가입',
+		'registered_label' => 'Registered:',
+		'registered_user' => '임시회원',
+		'remember' => '로그인 상태 유지',
+		'remember_label' => '로그인 상태 유지',
+		'remove_avatar' => 'Remove avatar:',
+		'reset_code_another' => '다시 보내기',
+		'reset_code_emailed' => '비밀번호 초기화에 필요한 코드를 이메일로 발송하였습니다',
+		'reset_code_label' => '코드:',
+		'reset_code_wrong' => '틀린 코드',
+		'reset_title' => '비밀번호 초기화',
+		'save_profile' => '회원 정보 저장',
+		'save_user' => 'Save User',
+		'send_confirm_button' => '본인 확인',
+		'send_password_button' => '임시 비밀번호 보내기',
+		'send_reset_button' => '비밀번호 초기화',
+		'send_reset_note' => '등록하신 이메일 주소로 비밀번호 초기화 이메일을 발송합니다.',
+		'special_users' => '관리자 계정',
+		'terms_not_accepted' => 'You must tick this.',
+		'unblock_user_button' => 'Unblock User',
+		'unsubscribe' => 'Unsubscribe:',
+		'unsubscribe_complete' => 'You have been unsubscribed from mass mailings sent out by ^0. You may resubscribe at any time via your ^1account^2 page.',
+		'unsubscribe_title' => 'Unsubscribe',
+		'unsubscribe_wrong_log_in' => 'Code not correct - please ^1log in^2 to unsubscribe',
+		'user_blocked' => '(blocked)',
+		'user_not_found' => '해당 사용자 계정이 없습니다',
+		'wall_posts' => 'Wall posts:',
+		'wall_posts_explanation' => 'Allow users to post on your wall (you will also be emailed)',
+		'website' => '홈페이지',
+		'x_ago_from_y' => '^1 전 ^2에서 접속',
+	);
+
+
+/*
+	Omit PHP closing tag to help avoid accidental output
+*/
